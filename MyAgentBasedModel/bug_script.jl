31eb2e5b7073e725b34c5155f88f3eaf5f2aa6b3
Alonso Martinez Cisneros <martinez@zib.de>
2023-09-15 17:40:01 +0200

    carrying out stochastic simulations

--- MyAgentBasedModel/bug_script.jl
+++ MyAgentBasedModel/bug_script.jl
@@ -2,7 +2,16 @@ using Pkg
 Pkg.activate(".")
 
 using MyAgentBasedModel
+using JLD2
 
-omp = OpinionModelProblem((-4, 4), (-4, 4))
+# omp = OpinionModelProblem((-2, 2), (-2, 2))
+# Loading the exact same initial state
+init_state = load("../new_settings.jld2")
+X, Y, Z, A, B, C = init_state["X"], init_state["Y"], init_state["Z"], init_state["A"], init_state["B"], init_state["C"]
+p = OpinionModelParams()
 
-solve(omp)
+omp = OpinionModelProblem(p, X, Y, Z, C, A, B)
+
+rX, rY, rZ, rC = solve(omp)
+
+jldsave("../stochastic_new.jld2"; rX)
